441f9b98b93dc90eab2f12d735a3c2fa6e43dbc6
cyliong <liongtheprogrammer@gmail.com>
2021-09-17 13:56:56 +0800

    Provide a const constructor for the App class

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:list_module/page/about_page.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
